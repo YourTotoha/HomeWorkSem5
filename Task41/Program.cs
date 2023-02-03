7b6002bc68f7b58587719762a895a48105dd7a01
Vladimir <saintprize@gmail.com>
2023-02-03 16:08:45 +0300

    add homework

---
+++ Task41/Program.cs
@@ -0,0 +1,35 @@
+﻿//Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 3
+
+void CountNumbers(int NumberM)
+{
+    int CountAbove0 = 0;
+    int FirstNumber = GetNum("Введите первое число: ");
+    if (FirstNumber > 0)
+    {
+        CountAbove0 = CountAbove0 + 1;
+    }
+    for (int i = 1; i < NumberM; i++)
+    {
+        int NextNumber = GetNum($"Введите {i + 1} число: ");
+        if (NextNumber > 0)
+        {
+            CountAbove0 = CountAbove0 + 1;
+        }
+    }
+    Console.Write($"Количество чисел, которые больше 0 = {CountAbove0}");
+}
+
+int GetNum(string text)
+{
+    Console.WriteLine(text);
+    int Number = int.Parse(Console.ReadLine());
+    return (Number);
+}
+
+Console.WriteLine("Программа работает следующим образом, вы вводите число - количество чисел, которые вы хотите ввести.\nВы по очередности их вводите. В конце опказывается количество чисел больше 0");
+
+int NumberM = GetNum("Введите количество чисел, которые вы хотите ввести M: ");
+
+CountNumbers(NumberM);
---
+++ Task43/Program.cs
@@ -0,0 +1,23 @@
+﻿//Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+//b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+void EquationIntersection(double NumberB1, double NumberK1, double NumberB2, double NumberK2)
+{
+    double NumberX = (NumberB1 - NumberB2) * -1 / (NumberK1 - NumberK2);
+    double NumberY = NumberK2 * NumberX + NumberB2;
+    Console.Write($"Точка пересечения имеет следующие координаты: ({NumberX}, {NumberY})");
+}
+
+int GetNum(string text)
+{
+    Console.WriteLine(text);
+    int Number = int.Parse(Console.ReadLine());
+    return (Number);
+}
+
+int NumberB1 = GetNum("Введите число для b1: ");
+int NumberK1 = GetNum("Введите число для k1: ");
+int NumberB2 = GetNum("Введите число для b2: ");
+int NumberK2 = GetNum("Введите число для k2: ");
+
+EquationIntersection(NumberB1, NumberK1, NumberB2, NumberK2);
